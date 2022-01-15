9f48754345954f5819f352dd27c39a182e09970c
Mandan Kazzazi <92727403+MandanK@users.noreply.github.com>
2022-01-15 22:21:39 +0100

    create the first version

---
+++ index.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const color = randomColor({
+  luminosity: process.argv[3],
+  hue: process.argv[2],
+});
+
+console.log(
+  chalk.hex(color).bold(`###############################
+###############################
+###############################
+#####                     #####
+#####       ${color}       #####
+#####                     #####
+###############################
+###############################
+###############################`),
+);
